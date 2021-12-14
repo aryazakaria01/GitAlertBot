6c7e9052a664440f05e20591a3498ddf7b75794a
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-14 09:47:57 +0700

    ok

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     CHAT_ID = int(os.environ.get("CHAT_ID", 0))
     PORT = int(os.environ.get("PORT", "8443"))
-    HOST = os.environ.get("HOST", "0.0.0.0")
+    HOST = os.environ.get("HOST", "127.0.0.1")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
     PING_TIME = int(os.environ.get("PING_TIME", 10))
